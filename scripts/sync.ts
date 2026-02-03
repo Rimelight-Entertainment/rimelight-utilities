9ab31da1bb790fe916e80c1b8cb01407f22d3993
idantitydotme <88442616+idantitydotme@users.noreply.github.com>
2026-02-03 16:30:15 -0300

    Adds syncing script

---
+++ scripts/sync.ts
@@ -0,0 +1,30 @@
+import { copyFile } from "node:fs/promises";
+import { join } from "node:path";
+
+// process.cwd() refers to the root of the project RUNNING the script
+const PROJECT_ROOT = process.cwd();
+// This assumes the submodule is always named .rimelight-utils
+const SHARED_ROOT = join(PROJECT_ROOT, ".rimelight-utilities");
+
+const FILES_TO_SYNC = [
+  ".gitignore",
+  ".editorconfig"
+];
+
+async function runSync() {
+  console.log("🔄 Syncing shared configs...");
+
+  for (const file of FILES_TO_SYNC) {
+    try {
+      await copyFile(
+          join(SHARED_ROOT, file),
+          join(PROJECT_ROOT, file)
+      );
+      console.log(`✅ ${file} synced`);
+    } catch (err) {
+      console.warn(`⚠️  Could not sync ${file}:`, err.message);
+    }
+  }
+}
+
+runSync();
